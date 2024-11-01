3fc8f4abb50e19b3d82dc9a96e8e622569f234d5
sophian <littlejohn.sophia@gmail.com>
2024-11-01 17:04:51 -0400

    Export context directly and introduce debug flag to prevent vn being deleted

--- src/tests/Centrifuge.test.ts
+++ src/tests/Centrifuge.test.ts
@@ -2,65 +2,53 @@ import { expect } from 'chai'
 import { combineLatest, filter, firstValueFrom, last, Observable } from 'rxjs'
 import { parseEther } from 'viem'
 import type { OperationConfirmedStatus } from '../types/transaction.js'
-import { withContext } from './setup.js'
+import { context } from './setup.js'
 
 describe('Centrifuge', () => {
-  it(
-    'should be connected to sepolia',
-    withContext(async function () {
-      const client = this.context.centrifuge.getClient()
-      expect(client?.chain.id).to.equal(11155111)
-      const chains = this.context.centrifuge.chains
-      expect(chains).to.include(11155111)
-    })
-  )
+  it('should be connected to sepolia', async function () {
+    const client = context.centrifuge.getClient()
+    expect(client?.chain.id).to.equal(11155111)
+    const chains = context.centrifuge.chains
+    expect(chains).to.include(11155111)
+  })
 
-  it(
-    'should fetch account and balances',
-    withContext(async function () {
-      const account = await this.context.centrifuge.account('0x423420Ae467df6e90291fd0252c0A8a637C1e03f')
-      const balances = await account.balances()
-      expect(balances).to.exist
-    })
-  )
+  it('should fetch account and balances', async function () {
+    const account = await context.centrifuge.account('0x423420Ae467df6e90291fd0252c0A8a637C1e03f')
+    const balances = await account.balances()
+    expect(balances).to.exist
+  })
 
-  it(
-    'should make a transfer',
-    withContext(async function () {
-      const fromAddress = this.context.tenderlyFork.account.address
-      const destAddress = '0x423420Ae467df6e90291fd0252c0A8a637C1e03f'
-      const transferAmount = 10_000_000n
+  it('should make a transfer', async function () {
+    const fromAddress = this.context.tenderlyFork.account.address
+    const destAddress = '0x423420Ae467df6e90291fd0252c0A8a637C1e03f'
+    const transferAmount = 10_000_000n
 
-      await Promise.all([
-        this.context.tenderlyFork.fundAccountEth(fromAddress, parseEther('100')),
-        this.context.tenderlyFork.fundAccountERC20(fromAddress, 100_000_000n),
+    await Promise.all([
+      context.tenderlyFork.fundAccountEth(fromAddress, parseEther('100')),
+      context.tenderlyFork.fundAccountERC20(fromAddress, 100_000_000n),
+    ])
+    const fromAccount = await context.centrifuge.account(fromAddress)
+    const destAccount = await context.centrifuge.account(destAddress)
+    const fromBalanceInitial = await fromAccount.balances()
+    const destBalanceInitial = await destAccount.balances()
+
+    const [transfer, fromBalanceFinal, destBalanceFinal] = await firstValueFrom(
+      combineLatest([
+        fromAccount.transfer(destAddress, transferAmount).pipe(last()) as Observable<OperationConfirmedStatus>,
+        fromAccount.balances().pipe(filter((balance) => balance !== fromBalanceInitial)),
+        destAccount.balances().pipe(filter((balance) => balance !== destBalanceInitial)),
       ])
-      const fromAccount = await this.context.centrifuge.account(fromAddress)
-      const destAccount = await this.context.centrifuge.account(destAddress)
-      const fromBalanceInitial = await fromAccount.balances()
-      const destBalanceInitial = await destAccount.balances()
+    )
 
-      const [transfer, fromBalanceFinal, destBalanceFinal] = await firstValueFrom(
-        combineLatest([
-          fromAccount.transfer(destAddress, transferAmount).pipe(last()) as Observable<OperationConfirmedStatus>,
-          fromAccount.balances().pipe(filter((balance) => balance !== fromBalanceInitial)),
-          destAccount.balances().pipe(filter((balance) => balance !== destBalanceInitial)),
-        ])
-      )
+    expect(transfer.type).to.equal('TransactionConfirmed')
+    expect(transfer.title).to.equal('Transfer')
+    expect(transfer.receipt.status).to.equal('success')
+    expect(fromBalanceFinal).to.equal(fromBalanceInitial - transferAmount)
+    expect(destBalanceFinal).to.equal(destBalanceInitial + transferAmount)
+  })
 
-      expect(transfer.type).to.equal('TransactionConfirmed')
-      expect(transfer.title).to.equal('Transfer')
-      expect(transfer.receipt.status).to.equal('success')
-      expect(fromBalanceFinal).to.equal(fromBalanceInitial - transferAmount)
-      expect(destBalanceFinal).to.equal(destBalanceInitial + transferAmount)
-    })
-  )
-
-  it(
-    'should fetch a pool by id',
-    withContext(async function () {
-      const pool = await this.context.centrifuge.pool('4139607887')
-      expect(pool).to.exist
-    })
-  )
+  it('should fetch a pool by id', async function () {
+    const pool = await context.centrifuge.pool('4139607887')
+    expect(pool).to.exist
+  })
 })
--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,14 +2,6 @@ import { Centrifuge } from '../Centrifuge.js'
 import { TenderlyFork } from './tenderly.js'
 import { sepolia } from 'viem/chains'
 
-declare global {
-  namespace Mocha {
-    interface Context {
-      context: TestContext & TestContext
-    }
-  }
-}
-
 class TestContext {
   public centrifuge!: Centrifuge
   public tenderlyFork!: TenderlyFork
@@ -27,34 +19,26 @@ class TestContext {
   }
 
   async cleanup() {
+    if (process.env.DEBUG === 'true') {
+      console.log('DEBUG is true, RPC endpoint will not be deleted', this.tenderlyFork.rpcUrl)
+      return
+    }
     if (this.tenderlyFork && this.allTestsSucceeded) {
-      await this.tenderlyFork.deleteTenderlyRpcEndpoint()
+      return this.tenderlyFork.deleteTenderlyRpcEndpoint()
     }
     console.log('A test has failed, RPC endpoint will not be deleted', this.tenderlyFork.rpcUrl)
   }
 }
 
-export function withContext(fn: (this: Mocha.Context & { context: TestContext }) => Promise<void>) {
-  return function (this: Mocha.Context) {
-    return fn.call(this)
-  }
-}
-
-const testContext = new TestContext()
+export const context = new TestContext()
 
 export const mochaHooks = {
   beforeAll: async function (this: Mocha.Context & TestContext) {
     this.timeout(30000) // Increase timeout for setup
-    await testContext.initialize()
-    this.context = testContext as TestContext
-  },
-  afterEach: async function (this: Mocha.Context & TestContext) {
-    // TODO: only keep the vn alive if a test involving a query/tx has failed
-    if (this.currentTest?.state === 'failed') {
-      this.context.allTestsSucceeded = false
-    }
+    await context.initialize()
+    this.context = context as TestContext
   },
   afterAll: async function (this: Mocha.Context & TestContext) {
-    await testContext.cleanup()
+    await context.cleanup()
   },
 }
