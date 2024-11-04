921352f3605d78bd246732d2c9ff3c2599ebecfa
sophian <littlejohn.sophia@gmail.com>
2024-11-04 17:51:37 -0500

    Fix failing test

--- src/tests/Account.test.ts
+++ src/tests/Account.test.ts
@@ -49,17 +49,16 @@ describe('Account', () => {
       context.tenderlyFork.fundAccountEth(impersonatedAddress, parseEther('100')),
       context.tenderlyFork.fundAccountERC20(impersonatedAddress, transferAmount),
     ])
-    const fromAccount = await context.centrifuge.account(impersonatedAddress)
-    const fromBalanceInitial = await fromAccount.balances()
 
+    const impersonatedAccount = await context.centrifuge.account(impersonatedAddress)
     const destAddress = '0x26876eAceb62d31214C31F1a58b74A1445018b75'
     const destAccount = await context.centrifuge.account(destAddress)
     const destBalanceInitial = await destAccount.balances()
 
-    const [transfer, fromBalanceFinal, destBalanceFinal] = await firstValueFrom(
+    const [transfer, impersonatedBalanceFinal, destBalanceFinal] = await firstValueFrom(
       combineLatest([
-        fromAccount.transfer(destAddress, transferAmount).pipe(last()) as Observable<OperationConfirmedStatus>,
-        fromAccount.balances().pipe(filter((balance) => balance !== fromBalanceInitial)),
+        impersonatedAccount.transfer(destAddress, transferAmount).pipe(last()) as Observable<OperationConfirmedStatus>,
+        impersonatedAccount.balances().pipe(filter((balance) => balance !== transferAmount)),
         destAccount.balances().pipe(filter((balance) => balance !== destBalanceInitial)),
       ])
     )
@@ -67,7 +66,7 @@ describe('Account', () => {
     expect(transfer.type).to.equal('TransactionConfirmed')
     expect(transfer.title).to.equal('Transfer')
     expect(transfer.receipt.status).to.equal('success')
-    // expect(fromBalanceFinal).to.equal(fromBalanceInitial - transferAmount)
-    // expect(destBalanceFinal).to.equal(destBalanceInitial + transferAmount)
+    expect(impersonatedBalanceFinal).to.equal(0n)
+    expect(destBalanceFinal).to.equal(destBalanceInitial + transferAmount)
   })
 })
