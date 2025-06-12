964764f6aea6da4241172218082db8679fdd58e3
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2025-06-12 15:54:00 +0200

    fix abi

--- src/abi/Valuation.abi.ts
+++ src/abi/Valuation.abi.ts
@@ -1,1 +1,1 @@
-export default ['function getQuote(uint128,address,address) external view returns (uint128)'] as const
+export default ['function getQuote(uint128,uin128,uin128) external view returns (uint128)'] as const
