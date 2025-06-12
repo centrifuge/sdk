ed91fa2e044be4925dcb0b4df0caebf02488c500
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2025-06-12 15:17:05 +0200

    abi

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -6,13 +6,13 @@ import CurrencyAbi from './Currency.abi.js'
 import HoldingsAbi from './Holdings.abi.js'
 import HubAbi from './Hub.abi.js'
 import HubRegistryAbi from './HubRegistry.abi.js'
-import IERC7726Abi from './IERC7726.abi.js'
 import MessageDispatcherAbi from './MessageDispatcher.abi.js'
 import MultiAdapterAbi from './MultiAdapter.abi.js'
 import RestrictionManagerAbi from './RestrictionManager.abi.js'
 import ShareClassManagerAbi from './ShareClassManager.abi.js'
 import SpokeAbi from './Spoke.abi.js'
 import SyncRequestsAbi from './SyncRequestManager.abi.js'
+import ValuationAbi from './Valuation.abi.js'
 import VaultRouterAbi from './VaultRouter.abi.js'
 
 export const ABI = {
@@ -27,7 +27,7 @@ export const ABI = {
   VaultRouter: parseAbi(VaultRouterAbi),
   Accounting: parseAbi(AccountingAbi),
   Holdings: parseAbi(HoldingsAbi),
-  IERC7726: parseAbi(IERC7726Abi),
+  Valuation: parseAbi(ValuationAbi),
   SyncRequests: parseAbi(SyncRequestsAbi),
   AsyncRequests: parseAbi(AsyncRequestsAbi),
   MultiAdapter: parseAbi(MultiAdapterAbi),
