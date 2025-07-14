e9352e78d7e8c986899c47cf8896a608087bf6cf
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2025-07-14 10:18:43 +0200

    fix currency

--- src/config/protocol.ts
+++ src/config/protocol.ts
@@ -3,6 +3,6 @@ import type { HexString } from '../types/index.js'
 
 export const currencies: Record<number, HexString[]> = {
   [sepolia.id]: ['0x3aaaa86458d576BafCB1B7eD290434F0696dA65c'],
-  [baseSepolia.id]: ['0x39F562Ff7da2863B0075dDF10c0fC0524C28ac92'],
+  [baseSepolia.id]: ['0x5da2060c5418223e7ef7bed788d9d3ea61f68034'],
   [arbitrumSepolia.id]: ['0x19cc063f962B2769012cA32B2A9027b5c76dCFf3'],
 }
