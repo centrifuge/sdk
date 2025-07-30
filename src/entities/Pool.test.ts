b939dba0063485024cfc96337bf1203e4fe57e3a
Gregory <wojciech@k-f.co>
2025-07-30 12:34:29 +0200

    finish tests

--- src/entities/Pool.test.ts
+++ src/entities/Pool.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { firstValueFrom, skip } from 'rxjs'
+import { firstValueFrom, skip, skipWhile } from 'rxjs'
 import { Centrifuge } from '../Centrifuge.js'
 import { NULL_ADDRESS } from '../constants.js'
 import { mockPoolMetadata } from '../tests/mocks/mockPoolMetadata.js'
@@ -8,6 +8,9 @@ import { randomAddress } from '../tests/utils.js'
 import { AssetId, PoolId, ShareClassId } from '../utils/types.js'
 import { Pool } from './Pool.js'
 import { PoolMetadataInput, ShareClassInput } from '../types/poolInput.js'
+import { ABI } from '../abi/index.js'
+import { getContract } from 'viem'
+import { PoolNetwork } from './PoolNetwork.js'
 
 const chainId = 11155111
 const centId = 1
@@ -108,10 +111,12 @@ describe('Pool', () => {
     expect(isNewManager).to.be.true
   })
 
-  it.only('updates a pool', async () => {
+  it('updates a pool', async () => {
+    const fakeHash = 'QmPdzJkZ4PVJ21HfBXMJbGopSpUP9C9fqu3A1f9ZVhtRY2'
+
     const metadataInput: Partial<PoolMetadataInput> = {
-      poolName: 'Test Pool',
-      poolStructure: 'revolving',
+      poolName: 'Updated Test Pool',
+      investorType: 'Fake Investor Type',
     }
 
     const addedShareClasses: ShareClassInput[] = [
@@ -141,16 +146,113 @@ describe('Pool', () => {
         apyPercentage: 6,
         apy: 'target',
         defaultAccounts: {
-          asset: 1000,
-          equity: 10000,
-          gain: 5000,
-          loss: 500,
+          asset: 3000,
+          equity: 30000,
+          gain: 4000,
+          loss: 400,
           expense: 1,
           liability: 2,
         },
       },
     ]
 
+    const expectedAccounts = [1, 2, 400, 500, 1000, 3000, 4000, 5000, 10000, 30000]
+
+    const centrifugeWithPin = new Centrifuge({
+      environment: 'testnet',
+      pinJson: async (data) => {
+        // Check if shareClasses data got updated correctly
+        expect(data.pool.name).to.equal('Updated Test Pool')
+        expect(data.pool.investorType).to.equal('Fake Investor Type')
+        expect(data.shareClasses['0x00010000000000010000000000000001']).to.deep.equal({
+          minInitialInvestment: 1000,
+          apy: 'target',
+          apyPercentage: 6,
+          weightedAverageMaturity: 49.42,
+          defaultAccounts: {
+            asset: 3000,
+            equity: 30000,
+            gain: 4000,
+            loss: 400,
+            expense: 1,
+            liability: 2,
+          },
+        })
+
+        // There is an issue with setup data, where poolDetails.shareClasses has two share classes, but poolDetails.metadata.shareClasses has only one.
+        // Until this is corrected, this count of 3 is expected.
+        expect(data.shareClasses['0x00010000000000010000000000000003']).to.deep.equal({
+          minInitialInvestment: 1000,
+          apy: 'target',
+          apyPercentage: 5,
+          defaultAccounts: {
+            asset: 1000,
+            equity: 10000,
+            gain: 5000,
+            loss: 500,
+            expense: 1,
+            liability: 2,
+          },
+        })
+
+        return fakeHash
+      },
+      rpcUrls: {
+        11155111: context.tenderlyFork.rpcUrl,
+      },
+    })
+
+    context.tenderlyFork.impersonateAddress = poolManager
+    centrifugeWithPin.setSigner(context.tenderlyFork.signer)
+
+    const pool = await centrifugeWithPin.pool(poolId)
+
     const result = await pool.updatePool(metadataInput, updatedShareClasses, addedShareClasses)
+
+    expect(result.type).to.equal('TransactionConfirmed')
+
+    // await poolDetails -> check extra share class included
+    const poolDetails = await firstValueFrom(
+      pool.details().pipe(skipWhile((details) => details.shareClasses[2]?.details == null))
+    )
+
+    expect(poolDetails.shareClasses.length).to.equal(3)
+
+    // Check if newly added share class is present with correct data
+    expect(poolDetails.shareClasses[2]!.details).to.contain({
+      name: 'Test Share Class2',
+      symbol: 'TSC2',
+    })
+
+    const { accounting } = await context.centrifuge._protocolAddresses(chainId)
+    const network = new PoolNetwork(context.centrifuge, pool, chainId)
+
+    // Share Token for updated share class
+    const shareToken = await network._share(poolDetails.shareClasses[0]!.shareClass.id)
+
+    const accountsContract = getContract({
+      address: accounting,
+      abi: ABI.Accounting,
+      client: context.centrifuge.getClient(chainId),
+    })
+
+    // Check if all accounts got created properly
+    for (const account of expectedAccounts) {
+      const exists = await accountsContract.read.exists([poolId.raw, account])
+      expect(exists).to.be.true
+    }
+
+    // Check if existing share class got updated
+    const shareClassContract = getContract({
+      address: shareToken,
+      abi: ABI.Currency,
+      client: context.centrifuge.getClient(chainId),
+    })
+
+    const updateShareClassName = await shareClassContract.read.name()
+    const updatedShareClassSymbol = await shareClassContract.read.symbol()
+
+    expect(updateShareClassName).to.equal('Sepolia Pool One Token2')
+    expect(updatedShareClassSymbol).to.equal('sep.poo.one2')
   })
 })
