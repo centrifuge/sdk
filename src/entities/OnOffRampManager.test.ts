6b091554347c540bda7c5380ad77be91e785c3e8
Gregory <wojciech@k-f.co>
2025-08-14 10:54:38 +0200

    add new OnOffRampManager class with tests, needs finishing

---
+++ src/entities/OnOffRampManager.test.ts
@@ -0,0 +1,193 @@
+import { expect } from 'chai'
+import { AssetId, PoolId, ShareClassId } from '../utils/types.js'
+import { OnOffRampManager } from './OnOffRampManager.js'
+import { Pool } from './Pool.js'
+import { context } from '../tests/setup.js'
+import { PoolNetwork } from './PoolNetwork.js'
+import { ShareClass } from './ShareClass.js'
+import { randomAddress } from '../tests/utils.js'
+import { ABI } from '../abi/index.js'
+import { Balance } from '../utils/BigInt.js'
+import { doTransaction } from '../utils/transaction.js'
+
+const chainId = 11155111
+const centId = 1
+const poolId = PoolId.from(centId, 1)
+const scId = ShareClassId.from(poolId, 1)
+const assetId = AssetId.from(centId, 1)
+
+const rampManager = '0x8c0E6DC2461c6190A3e5703B714942cacfCb3C14'
+const fundManager = '0x423420Ae467df6e90291fd0252c0A8a637C1e03f'
+const receiver = '0x0000000000000000000000000000a3df6c4f8fcf'
+
+describe('OnOffRampManager', () => {
+  let pool: Pool
+  let poolNetwork: PoolNetwork
+  let shareClass: ShareClass
+
+  let onOffRampManager: OnOffRampManager
+
+  before(async () => {
+    const { centrifuge } = context
+    pool = new Pool(centrifuge, poolId.raw, chainId)
+    poolNetwork = new PoolNetwork(centrifuge, pool, 11155111)
+    shareClass = new ShareClass(centrifuge, pool, scId.raw)
+
+    context.tenderlyFork.impersonateAddress = fundManager
+    context.centrifuge.setSigner(context.tenderlyFork.signer)
+
+    onOffRampManager = new OnOffRampManager(context.centrifuge, poolNetwork, shareClass, rampManager)
+  })
+
+  describe('receivers', () => {
+    it('should return receivers', async () => {
+      const receivers = await onOffRampManager.receivers()
+
+      // TODO: assert once the data is available
+      console.log({ receivers })
+    })
+
+    it('should set receiver', async () => {
+      const address = randomAddress()
+
+      const result = await onOffRampManager.setReceiver(assetId, address)
+
+      expect(result.type).to.equal('TransactionConfirmed')
+    })
+  })
+
+  describe('relayers', () => {
+    it('should return relayers', async () => {
+      const relayers = await onOffRampManager.relayers()
+
+      expect(relayers.length).to.equal(1)
+      expect(relayers[0]!.address).to.equal('0x000000000000000000000000000ffc2d83c1400c')
+    })
+
+    it('should set relayer', async () => {
+      const address = randomAddress()
+
+      const result = await onOffRampManager.setRelayer(address)
+
+      expect(result.type).to.equal('TransactionConfirmed')
+    })
+  })
+
+  describe('assets', () => {
+    it('should return assets', async () => {
+      const result = await onOffRampManager.assets()
+
+      expect(result.length).to.be.equal(1)
+
+      expect(result[0]!).to.deep.equal({
+        assetAddress: '0x3aaaa86458d576bafcb1b7ed290434f0696da65c',
+        centrifugeId: '1',
+        poolId: '281474976710657',
+        tokenId: '0x00010000000000010000000000000001',
+      })
+    })
+
+    it('should set asset', async () => {
+      const result = await onOffRampManager.setAsset(assetId)
+
+      expect(result.type).to.equal('TransactionConfirmed')
+    })
+  })
+
+  describe('balances', () => {
+    it('should return balances per asset', async () => {
+      const result = await onOffRampManager.balances()
+
+      console.log({ result })
+
+      // TODO: assert once the data is available, with balance greater than 0
+
+      // balance: Balance { value: 0n, decimals: 6 },
+      // currency: {
+      // address: '0x3aaaa86458d576bafcb1b7ed290434f0696da65c',
+      // tokenId: 0n,
+      // decimals: 6,
+      // name: 'USD Coin',
+      // symbol: 'USDC',
+      // chainId: 11155111,
+      // supportsPermit: true
+      // }
+    })
+  })
+
+  describe.only('deposit and withdraw', () => {
+    it('should deposit', async () => {
+      const assetAddress = '0x3aaaa86458d576bafcb1b7ed290434f0696da65c'
+      const allowance = new Balance(100n, 6)
+      const amount = new Balance(56n, 6)
+
+      await pool.updateBalanceSheetManagers([{ chainId, address: onOffRampManager.onrampAddress, canManage: true }])
+
+      await context.centrifuge._transact(async function* (ctx) {
+        yield* doTransaction('Add share class', ctx.publicClient, async () => {
+          return ctx.walletClient.writeContract({
+            address: assetAddress,
+            abi: ABI.Currency,
+            functionName: 'transfer',
+            args: [onOffRampManager.onrampAddress, allowance.toBigInt()],
+          })
+        })
+      }, chainId)
+
+      const result = await onOffRampManager.deposit(assetAddress, amount, receiver)
+
+      expect(result.type).to.equal('TransactionConfirmed')
+
+      const balance = await context.centrifuge.getClient(chainId).readContract({
+        address: assetAddress,
+        abi: ABI.Currency,
+        functionName: 'balanceOf',
+        args: [onOffRampManager.onrampAddress],
+      })
+
+      expect(balance).to.equal(allowance.sub(amount).toBigInt())
+    })
+
+    it('should withdraw', async () => {
+      const assetAddress = '0x3aaaa86458d576bafcb1b7ed290434f0696da65c'
+      const allowance = new Balance(100n, 6)
+      const amount = new Balance(56n, 6)
+
+      await pool.updateBalanceSheetManagers([{ chainId, address: onOffRampManager.onrampAddress, canManage: true }])
+
+      await context.centrifuge._transact(async function* (ctx) {
+        yield* doTransaction('Add share class', ctx.publicClient, async () => {
+          return ctx.walletClient.writeContract({
+            address: assetAddress,
+            abi: ABI.Currency,
+            functionName: 'transfer',
+            args: [onOffRampManager.onrampAddress, allowance.toBigInt()],
+          })
+        })
+      }, chainId)
+
+      const receiverBalanceBefore = await context.centrifuge.getClient(chainId).readContract({
+        address: assetAddress,
+        abi: ABI.Currency,
+        functionName: 'balanceOf',
+        args: [receiver],
+      })
+
+      expect(receiverBalanceBefore).to.equal(0n)
+
+      // TODO: Tenderly is saying that there is no contract code for withdraw?
+      const result = await onOffRampManager.withdraw(assetAddress, amount, receiver)
+
+      expect(result.type).to.equal('TransactionConfirmed')
+
+      const receiverBalanceAfter = await context.centrifuge.getClient(chainId).readContract({
+        address: assetAddress,
+        abi: ABI.Currency,
+        functionName: 'balanceOf',
+        args: [receiver],
+      })
+
+      console.log({ receiverBalanceAfter })
+    })
+  })
+})
