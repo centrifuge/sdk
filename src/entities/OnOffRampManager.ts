5f321fe1e9b41b9a767a8dc040af46f954e52fe6
Katty Barroso <kattybarroso1321@gmail.com>
2025-09-11 11:19:49 +0200

    fix: dummy change

--- src/entities/OnOffRampManager.ts
+++ src/entities/OnOffRampManager.ts
@@ -97,6 +97,9 @@ export class OnOffRampManager extends Entity {
     )
   }
 
+  /**
+   * Get the assets of an OnOffRampManager.
+   */
   assets() {
     return this._query(['assets'], () =>
       this._root.id(this.network.chainId).pipe(
