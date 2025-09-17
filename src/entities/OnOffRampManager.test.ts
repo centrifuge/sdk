94ee913503ff49cfeaaca575cc6e406da0d12887
Gregory <wojciech@k-f.co>
2025-09-17 10:30:58 +0200

    fix failing test

--- src/entities/OnOffRampManager.test.ts
+++ src/entities/OnOffRampManager.test.ts
@@ -69,7 +69,7 @@ describe('OnOffRampManager', () => {
     it('should return relayers', async () => {
       const relayers = await onOffRampManager.relayers()
 
-      expect(relayers.length).to.equal(1)
+      expect(relayers.length).to.equal(3)
       expect(relayers[0]!.address).to.equal('0x000000000000000000000000000ffc2d83c1400c')
     })
 
