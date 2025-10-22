25e0b99b5c1c5b0bd9abd74f5027a0dc94123dd5
Katty Barroso <kattybarroso1321@gmail.com>
2025-10-22 09:33:54 +0200

    feat: add bnb chain

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,3 +1,18 @@
 import { arbitrum, arbitrumSepolia, avalanche, base, baseSepolia, mainnet, plumeMainnet, sepolia } from 'viem/chains'
 
+<<<<<<< Updated upstream
 export const chains = [mainnet, sepolia, base, baseSepolia, arbitrum, arbitrumSepolia, avalanche, plumeMainnet]
+=======
+export const chains = [
+  mainnet,
+  sepolia,
+  base,
+  baseSepolia,
+  arbitrum,
+  arbitrumSepolia,
+  avalanche,
+  plumeMainnet,
+  bsc,
+  bscTestnet,
+]
+>>>>>>> Stashed changes
