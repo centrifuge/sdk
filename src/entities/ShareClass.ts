778436b0327000e58634c761b0484c4fc1ece2b4
Katty Barroso <kattybarroso1321@gmail.com>
2025-11-11 10:38:30 +0100

    fix: chain id

--- src/entities/ShareClass.ts
+++ src/entities/ShareClass.ts
@@ -1439,7 +1439,6 @@ export class ShareClass extends Entity {
    * @returns Closed investment orders where shares have been issued
    */
   closedInvestments() {
-    const self = this
     return this._query(['closedInvestments'], () =>
       this._root._queryIndexer(
         `query ($scId: String!) {
@@ -1462,8 +1461,11 @@ export class ShareClass extends Entity {
               symbol
               name
             }
-            token {
+             token {
               decimals
+              blockchain {
+               chainId
+              }
             }
           }
         }
@@ -1491,6 +1493,9 @@ export class ShareClass extends Entity {
               }
               token: {
                 decimals: number
+                blockchain: {
+                  chainId: number
+                }
               }
             }[]
           }
@@ -1512,6 +1517,7 @@ export class ShareClass extends Entity {
               name: order.asset.name,
               decimals: order.asset.decimals,
             },
+            chainId: order.token.blockchain.chainId,
           }))
         }
       )
@@ -1523,7 +1529,6 @@ export class ShareClass extends Entity {
    * @returns Closed redemption orders where shares have been revoked
    */
   closedRedemptions() {
-    const self = this
     return this._query(['closedRedemptions'], () =>
       this._root._queryIndexer(
         `query ($scId: String!) {
@@ -1548,6 +1553,9 @@ export class ShareClass extends Entity {
             }
             token {
               decimals
+              blockchain {
+               chainId
+              }
             }
           }
         }
@@ -1575,6 +1583,9 @@ export class ShareClass extends Entity {
               }
               token: {
                 decimals: number
+                blockchain: {
+                  chainId: number
+                }
               }
             }[]
           }
@@ -1596,6 +1607,7 @@ export class ShareClass extends Entity {
               name: order.asset.name,
               decimals: order.asset.decimals,
             },
+            chainId: order.token.blockchain.chainId,
           }))
         }
       )
