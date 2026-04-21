f056ce6f274f3657cf1fe968d173cd8f2afa1752
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2026-04-21 14:25:16 +0200

    feat: add config for pharos chain

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -14,6 +14,28 @@ import {
   plumeMainnet,
   sepolia,
 } from 'viem/chains'
+import { defineChain } from 'viem'
+
+export const pharos = defineChain({
+  id: 1672,
+  name: 'Pharos Mainnet',
+  nativeCurrency: {
+    name: 'PharosCoin',
+    symbol: 'PROS',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.pharos.xyz'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Pharos Explorer',
+      url: 'https://pharosscan.xyz',
+    },
+  },
+})
 
 // TODO: convert to use the indexer to avoid hard coding
 export const chains = [
@@ -29,6 +51,7 @@ export const chains = [
   mainnet,
   monad,
   optimism,
+  pharos,
   plumeMainnet,
   sepolia,
 ]
