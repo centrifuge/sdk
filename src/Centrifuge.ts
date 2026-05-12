9f901f82d4bcc5e073a9f199fbfb02df48c5fecb
gregory-kf <wojciech@centrifuge.io>
2026-05-12 10:46:07 +0200

    chore: update default indexer url (#442)
    * update default indexer url
    
    * [bot] New pkg version: 1.8.5
    
    * Trigger Build
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- src/Centrifuge.ts
+++ src/Centrifuge.ts
@@ -82,7 +82,7 @@ const envConfig = {
     ...createPinning(PINNING_API_DEMO),
   },
   testnet: {
-    indexerUrl: 'https://api-v3-hitz.marble.live/graphql',
+    indexerUrl: 'https://api-v3-test.cfg.embrio.tech/graphql',
     ipfsUrl: 'https://ipfs.centrifuge.io',
     ...createPinning(PINNING_API_DEMO),
   },
